af36e27841f5dd61d4a2d659eceea2981c9b35fa
Glen <glenlcathey@gmail.com>
2022-03-16 16:41:55 -0500

    add error catch to server and write error to log

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const http = require('http');
-const app = require('./app.js');
+try {
+    const http = require('http');
+    const fs = require('fs');
+    const { getSystemErrorMap } = require('util');
+    const app = require('./app.js');
 
-const port = process.env.port || 3000;
+    const port = process.env.port || 3000;
 
-const server = http.createServer(app);
+    const server = http.createServer(app);
 
-server.listen(port);
+    server.listen(port);
+} catch( error ) {
+    fs.writeFile(__dirname + '/error_file.txt', error);
+}
