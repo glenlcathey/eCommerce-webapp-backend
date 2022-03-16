25ba90f846aa3ce2cc8c2634de2ce3e2a53f7965
Glen <glenlcathey@gmail.com>
2022-03-16 16:48:13 -0500

    add validating and default values to models

--- api/models/order.js
+++ api/models/order.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    product_id: mongoose.Schema.Types.ObjectId,
-    quantity: Number
+    _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    product_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    quantity: { type: Number, required: false, default: 1 }
 })
 
 module.exports = mongoose.model('Order', orderSchema);
