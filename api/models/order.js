d655826158789a4bb0a4b14ac582aa035ce42c8d
Glen <glenlcathey@gmail.com>
2022-03-19 14:36:59 -0500

    Order model now references Product model for product_id

--- api/models/order.js
+++ api/models/order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, required: true },
-    product_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    product_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
     quantity: { type: Number, required: false, default: 1 }
 })
 
