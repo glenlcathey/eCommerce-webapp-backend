91199a782f911c7c53dc6da6ccb0e9b2c9cc331a
Glen <glenlcathey@gmail.com>
2022-03-20 19:12:11 -0500

    make uploads folder static, product images publically available

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb+srv://shop-test:testingpassword@test-shop.lcooo.mongod
     });
 
 app.use(morgan("dev"));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
