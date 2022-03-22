b783e3da34aab4983b78be1c957faf846f791853
Glen <glenlcathey@gmail.com>
2022-03-21 20:03:28 -0500

    add jwt  generation and return to successful user login

--- api/routes/users.js
+++ api/routes/users.js
@@ -63,13 +63,16 @@ router.post('/login', (req, res, next) => {
                         });
                     }
                     if (result) {
-                        jwt.sign({
-                            email: user[0].email,
-                            userId: user[0]._id,
-
-                        })
+                        const token = jwt.sign({
+                                email: user[0].email,
+                                userId: user[0]._id
+                            }, process.env.JWT_KEY,
+                            {
+                                expiresIn: "1h"
+                            });
                         return res.status(200).json({
-                            message: "Auth successful!"
+                            message: "Auth successful!",
+                            token: token
                         });
                     }
                     return res.status(401).json({
